a3d0e9ff0756c290b52013195cd0bc9ae52c6387
Yash Yenurkar <yash.yenurkar@oracle.com>
2026-06-06 22:09:17 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 import sys
 
 def main():
-    sys.stdout.write("$ ")
-    command = input()
-    print(f"{command}: command not found")
+    while(True):
+        sys.stdout.write("$ ")
+        command = input()
+        print(f"{command}: command not found")
 
 
 if __name__ == "__main__":
